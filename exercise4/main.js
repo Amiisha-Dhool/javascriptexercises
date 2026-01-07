779c3ea731e44037b7c0931a2a414de36c22efad
Amiisha-Dhool <dhoolpari5@gamil.com>
2026-01-07 20:56:59 +0300

    exercise4

---
+++ exercise4/main.js
@@ -0,0 +1,23 @@
+
+ function add(a,b){
+    console.log(a+b)
+ }
+ 
+
+ add(20,20)
+ add(2,5)
+ add(8,5)
+ add(2,18)
+
+
+
+ //function expretion
+ const greet = function(name){
+    console.log("hello" +name)
+ }
+
+ greet("dhool")
+ greet("amiish")
+ greet("huda")
+ greet("cali")
+ greet("caash")
