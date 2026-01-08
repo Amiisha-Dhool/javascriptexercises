39c803aea805363223ff2dd47af927f36818c17b
Amiisha-Dhool <dhoolpari5@gamil.com>
2026-01-09 02:14:17 +0300

    exercise8

---
+++ exercise8/main.js
@@ -0,0 +1,12 @@
+let item = {
+    make: "sout",
+    model:"latest",
+    year:"2026"
+}
+
+console.log(item.make)
+console.log(item.model)
+console.log(item.year)
+console.log(item["make"])
+console.log(item["model"])
+console.log(item["year"])
