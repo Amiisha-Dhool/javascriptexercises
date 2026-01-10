292eea1db5af4629eb019ef56720773963931179
Amiisha-Dhool <dhoolpari5@gamil.com>
2026-01-10 13:42:25 +0300

    exercise9

---
+++ exercise10/main.js
@@ -0,0 +1,24 @@
+
+
+let libarary = [
+     {
+        title: "saaxib",
+        author: "adan sahan",
+        year: "2020"
+     },
+     {
+        
+        title: "muraad",
+        author: "dhool",
+        year: "2025"
+     },
+     {
+         title: "gardso intaadan gursan",
+        author: "mubrik haadi",
+        year: "2019"
+     }
+]
+
+console.log(libarary)
+console.log(libarary[0].title)
+console.log(libarary[1].author)
