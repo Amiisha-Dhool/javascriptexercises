e231d9d48f8d0313c1301299d250d6014a49e82f
Amiisha-Dhool <dhoolpari5@gamil.com>
2026-01-10 13:17:04 +0300

    exercise9

---
+++ exercise9/main.js
@@ -0,0 +1,17 @@
+
+let person = {
+    car: "v8",
+    city: "jigjiga",
+    region:"5",
+
+
+    //markad function racinysid     
+    //arow functionka ma shaqynayo
+    start:function() {
+  console.log("the car has started ", this.car)
+    }
+}
+
+console.log(person)
+console.log(person.start())
+
