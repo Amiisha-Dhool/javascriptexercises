39f03c0e6a2a5430b2fc7e757b5de4e76309cd49
Amiisha-Dhool <dhoolpari5@gamil.com>
2026-01-11 15:58:15 +0300

    exercise17

---
+++ exercise17/main.js
@@ -0,0 +1,14 @@
+
+
+let tempereture = 46;
+
+if(tempereture  < 0){
+    console.log('verry cold')
+}else if(tempereture <15){
+     console.log('cold')
+}else if (tempereture <25){
+     console.log('warm')
+}else if(tempereture >25){
+     console.log('hot')
+}
+
