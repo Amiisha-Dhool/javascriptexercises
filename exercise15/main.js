20e38dd0a066252a10d5b961be8681bedd09ad7c
Amiisha-Dhool <dhoolpari5@gamil.com>
2026-01-11 13:52:11 +0300

    exercise15

---
+++ exercise15/main.js
@@ -0,0 +1,20 @@
+const people = [
+
+    {name:"huda", age:12, citty:"jigjiga", class:"8b"},
+
+    {name:"dhool", age:20, citty:"dhagaxbuur", class:"4a"},
+
+    {name:"hodan", age:22, citty:"hargysa", class:"8a"}
+
+
+    
+]
+
+for(let peoples of people){
+    console.log(peoples)
+}
+
+
+for(let key in people){
+    console.log(people[key])
+}
