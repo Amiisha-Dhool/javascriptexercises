8846537dbce594546870a82698cb9da5e2a81f9c
Amiisha-Dhool <dhoolpari5@gamil.com>
2026-01-22 08:25:31 +0300

    exercise32

---
+++ exercise32/main.js
@@ -0,0 +1,15 @@
+
+
+
+
+const header = document.querySelector('.header')
+
+console.log(header);
+
+const p = document.querySelectorAll('p')
+console.log(p)
+
+
+const button = document.querySelector('#button')
+
+console.log(button)
