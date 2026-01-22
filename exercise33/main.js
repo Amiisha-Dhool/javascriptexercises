9644399783df6848c4a08cbf8add9dc2a81a1737
Amiisha-Dhool <dhoolpari5@gamil.com>
2026-01-22 09:18:32 +0300

    exercise33

---
+++ exercise33/main.js
@@ -0,0 +1,20 @@
+
+
+
+
+
+const header = document.querySelector('.header')
+
+console.log(header)
+
+
+const text = document.querySelector('#text')
+console.log(text)
+
+function changeContent(){
+    header.textContent = "dhool pari"
+}
+
+function changeElement(){
+    text.innerHTML = `welcome to my project <strong>subscripe my chanel</strong`
+}
